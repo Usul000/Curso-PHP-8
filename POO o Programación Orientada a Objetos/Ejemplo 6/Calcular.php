0efe4de6a8aaa9f547c331f7aef68c1c29098050
Iván Cernadas <ivancernadas@gmail.com>
2026-07-25 13:50:10 +0200

    Add files via upload

---
+++ POO o Programación Orientada a Objetos/Ejemplo 6/Calcular.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Interfaces;
+
+interface Calcular
+{
+    public function raizCuadrada(float $numero): float;
+    public function potencia(int $numero, int $potencia): int;
+}
+
+?>
---
+++ POO o Programación Orientada a Objetos/Ejemplo 6/OperacionesBasicas.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Interfaces;
+
+interface OperacionesBasicas
+{
+    public function calculosBasicos(int $numero1, int $numero2, string $mensaje): string;
+}
+?>
